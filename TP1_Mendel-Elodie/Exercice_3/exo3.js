0088e2bf77724d9e6c92a152865e4a1108eacafe
elodie <elodie.mendel@gmail.com>
2021-01-06 12:22:41 +0100

    modify exos

--- TP1_Mendel-Elodie/Exercice_3/exo3.js
+++ TP1_Mendel-Elodie/Exercice_3/exo3.js
@@ -1,20 +1,17 @@
 if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', function(event){
-    setTimeout(function () {
   	  document.getElementById("doeSupported").innerText = "Supported!";
-  	  //document.getElementById("x").innerHtml = event.X;
-  	  //document.getElementById("y").innerHtml = event.Y;
-  	  //document.getElementById("z").innerHtml = event.Z;
+  	  document.getElementById("x").innerHtml = event.X;
+  	  document.getElementById("y").innerHtml = event.Y;
+  	  document.getElementById("z").innerHtml = event.Z;
   	  document.getElementById("alpha").innerHtml = event.alpha;
   	  document.getElementById("beta").innerHtml = event.beta;
   	  document.getElementById("gamma").innerHtml = event.gamma;
-    }, 1000);
   });
 }
 
 if (window.DeviceMotionEvent) {
   window.addEventListener('devicemotion', function(event){
-    setTimeout(function () {
   	   let rx,ry,rz;
        let ax,ay,az;
             
@@ -28,6 +25,5 @@ if (window.DeviceMotionEvent) {
 
        document.getElementById("rotation").innerHtml = "[" + rx + "," +ry +"," +rz + "]";
   	   document.getElementById("acceleration").innerHtml = "[" + ax + "," +ay +"," +az + "]";
-    }, 1000);
   });
 }
--- TP1_Mendel-Elodie/Exercice_4/exo4.js
+++ TP1_Mendel-Elodie/Exercice_4/exo4.js
@@ -3,9 +3,10 @@ document.addEventListener("touchend", touche, false);
 document.addEventListener("touchcancel", touche, false);
 document.addEventListener("touchleave", touche, false);
 document.addEventListener("touchmove", touche, false);
+
     
 function touche(event) {
-	document.getElementById("x").innerHTML = event.changedTouches[0].pageX);
-	document.getElementById("y").innerHTML = event.changedTouches[0].pageY);
-	document.getElementById("typeEvent").innerHTML = event.type);
+	document.getElementById("x").innerHTML = event.changedTouches[0].pageX;
+	document.getElementById("y").innerHTML = event.changedTouches[0].pageY;
+	document.getElementById("typeEvent").innerHTML = event.type;
 }
