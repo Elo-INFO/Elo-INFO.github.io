08ab07726c7d352706b32a65c1a6370d989735cf
elodie <elodie.mendel@gmail.com>
2021-01-06 11:31:26 +0100

    modify TP1-exo3

--- TP1_Mendel-Elodie/Exercice_3/exo3.js
+++ TP1_Mendel-Elodie/Exercice_3/exo3.js
@@ -1,29 +1,33 @@
 if (window.DeviceOrientationEvent) {
   window.addEventListener('deviceorientation', function(event){
-  	document.getElementById("doeSupported").innerText = "Supported!";
-  	document.getElementById("x").innerHtml = event.X;
-  	document.getElementById("y").innerHtml = event.Y;
-  	document.getElementById("z").innerHtml = event.Z;
-  	document.getElementById("alpha").innerHtml = event.alpha;
-  	document.getElementById("beta").innerHtml = event.beta;
-  	document.getElementById("gamme").innerHtml = event.gamma;
+    setTimeout(function () {
+  	  document.getElementById("doeSupported").innerText = "Supported!";
+  	  document.getElementById("x").innerHtml = event.X;
+  	  document.getElementById("y").innerHtml = event.Y;
+  	  document.getElementById("z").innerHtml = event.Z;
+  	  document.getElementById("alpha").innerHtml = event.alpha;
+  	  document.getElementById("beta").innerHtml = event.beta;
+  	  document.getElementById("gamme").innerHtml = event.gamma;
+    }, 1000);
   });
 }
 
 if (window.DeviceMotionEvent) {
   window.addEventListener('devicemotion', function(event){
-  	let rx,ry,rz;
-    let ax,ay,az;
+    setTimeout(function () {
+  	   let rx,ry,rz;
+       let ax,ay,az;
             
-    rx = event.acceleration.x;
-    ry = event.acceleration.y;
-    rz = event.acceleration.z;
+       rx = event.acceleration.x;
+       ry = event.acceleration.y;
+       rz = event.acceleration.z;
             
-    ax = event.rotationRate.alpha;
-    ay = event.rotationRate.beta;
-    az = event.rotationRate.gamma;
+       ax = event.rotationRate.alpha;
+       ay = event.rotationRate.beta;
+       az = event.rotationRate.gamma;
 
-    document.getElementById("rotation").innerHtml = "[" + rx + "," +ry +"," +rz + "]";
-  	document.getElementById("acceleration").innerHtml = "[" + ax + "," +ay +"," +az + "]";
+       document.getElementById("rotation").innerHtml = "[" + rx + "," +ry +"," +rz + "]";
+  	   document.getElementById("acceleration").innerHtml = "[" + ax + "," +ay +"," +az + "]";
+    }, 1000);
   });
 }
