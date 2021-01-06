84344ba9f4bae099265050402b34d2a9c4da545f
elodie <elodie.mendel@gmail.com>
2021-01-06 11:37:49 +0100

    Modidy exos TP1

--- TP1_Mendel-Elodie/Exercice_2/geolocalisation.js
+++ TP1_Mendel-Elodie/Exercice_2/geolocalisation.js
@@ -24,13 +24,13 @@ navigator.geolocation.getCurrentPosition(function(pos) {
         document.getElementById("precision").innerHTML = crd.accuracy;
         document.getElementById("vitesse").innerHTML = crd.speed;
         document.getElementById("timestamp").innerHTML = pos.timestamp;
-    });
+});
     
-    wp = navigator.geolocation.watchPosition(function(pos) {
+wp = navigator.geolocation.watchPosition(function(pos) {
         let crd = pos.coords;
         document.getElementById("longitude").innerHTML = crd.longitude;
         document.getElementById("latitude").innerHTML = crd.latitude;
         document.getElementById("precision").innerHTML = crd.accuracy;
         document.getElementById("vitesse").innerHTML = crd.speed;
         document.getElementById("timestamp").innerHTML = pos.timestamp
-    });
+      });
