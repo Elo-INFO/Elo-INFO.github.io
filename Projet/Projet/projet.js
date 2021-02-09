f3414791c5dd67df72f2579245072752cdfd28c6
elodie <elodie.mendel@gmail.com>
2021-02-09 12:04:32 +0100

    add svg

--- Projet/Projet/projet.js
+++ Projet/Projet/projet.js
@@ -23,7 +23,7 @@ $(document).ready(function() {
     ctx.restore();
   }
 
-  var img = document.getElementById('arrow');
+  var img = document.getElementById('needle');
   if(window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', function(orientation) {
       img.style.transform = 'rotate(' + orientation.alpha + 'deg)' ;
