7a31c3f31a357d76d40f48dfcaff013d28ccff41
elodie <elodie.mendel@gmail.com>
2021-02-09 10:26:39 +0100

    Add project

---
+++ Projet/Projet/projet.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+
+  var canvas = document.getElementById('canvas') ;
+  var ctx = canvas.getContext('2d');
+  var compass = new Image();
+  var needle = new Image() ;
+  compass.src = 'img/compass.png' ;
+  needle.src = 'img/needle.png' ;
+
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      changeOrientation(orientation);
+    });
+  }
+
+  function changeOrientation(orientation) {
+    console.log(orientation) ;
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.drawImage(compass, 0, 0);
+    ctx.save();
+    ctx.translate(100, 100);
+    ctx.rotate(orientation.alpha * (Math.PI / 180));
+    ctx.drawImage(needle, -100, -100);
+    ctx.restore();
+  }
+
+}) ;
