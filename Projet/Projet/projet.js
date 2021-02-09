1b5600a435b6bc2cc45138fb6fbd5825cf81af98
elodie <elodie.mendel@gmail.com>
2021-02-09 11:52:51 +0100

    svg project

--- Projet/Projet/projet.js
+++ Projet/Projet/projet.js
@@ -1,11 +1,11 @@
 $(document).ready(function() {
 
-  var canvas = document.getElementById('canvas') ;
+  var canvas = document.getElementById('canvas');
   var ctx = canvas.getContext('2d');
   var compass = new Image();
   var needle = new Image() ;
-  compass.src = 'img/compass.png' ;
-  needle.src = 'img/needle.png' ;
+  compass.src = 'img/compass.png';
+  needle.src = 'img/needle.png';
 
   if(window.DeviceOrientationEvent) {
     window.addEventListener('deviceorientation', function(orientation) {
@@ -14,9 +14,8 @@ $(document).ready(function() {
   }
 
   function changeOrientation(orientation) {
-    console.log(orientation) ;
     ctx.clearRect(0, 0, canvas.width, canvas.height);
-    ctx.drawImage(compass, 150, 150);
+    ctx.drawImage(compass, 0, 0);
     ctx.save();
     ctx.translate(100, 100);
     ctx.rotate(orientation.alpha * (Math.PI / 180));
@@ -24,4 +23,11 @@ $(document).ready(function() {
     ctx.restore();
   }
 
-}) ;
+  var img = document.getElementById('arrow');
+  if(window.DeviceOrientationEvent) {
+    window.addEventListener('deviceorientation', function(orientation) {
+      img.style.transform = 'rotate(' + orientation.alpha + 'deg)' ;
+    }) ;
+  }
+
+});
